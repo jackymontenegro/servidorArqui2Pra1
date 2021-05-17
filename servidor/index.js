770543896b0ad3dcd0bb96c0213c2dfa82888164
jackymontenegro <3007065960101@ingenieria.usac.edu.gt>
2021-05-16 21:40:44 -0600

    peso

--- servidor/index.js
+++ servidor/index.js
@@ -2201,7 +2201,7 @@ app.post('/ultimoEntrenamiento/', function(req, res){/*Verificar el estado del u
     
       var usuario = req.body;
   
-        var sql = "select distinct dayname(fecha) as fecha, peso from peso where usuario_idusuario = "+parseInt(usuario.idusuario) ;
+        var sql = "select  dayname(fecha) as fecha, max(peso) from peso where usuario_idusuario = "+parseInt(usuario.idusuario)+" group by  dayname(fecha)" ;
         console.log(sql);
         mysqlConnection.query(sql,(err, rows,fields)=>{
 
